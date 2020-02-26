435606b01da9a4064bf501a68d9d3aaab3661142
Christian <christian.ceccar@gmail.com>
2020-02-25 19:40:04 -0500

    fixed message sending stuff

---
+++ Assignment1/Client/Client.cpp
@@ -0,0 +1,115 @@
+// UDP Client
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+
+
+	//Initialize winsock
+	WSADATA wsa;
+
+	int error;
+	error = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	if (error != 0) {
+		printf("Failed to initialize %d\n", error);
+		return 1;
+	}
+
+	
+
+
+
+	//Create a client socket
+
+	struct addrinfo* ptr = NULL, hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+
+	std::string ipAdd;
+	printf("Please enter IP Address: \n");
+	std::getline(std::cin, ipAdd);
+
+	if (getaddrinfo(ipAdd.c_str(), "8888", &hints, &ptr) != 0) {
+		printf("Getaddrinfo failed!! %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+	
+
+	
+
+	SOCKET cli_socket;
+
+	cli_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (cli_socket == INVALID_SOCKET) {
+		printf("Failed creating a socket %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+	else {
+		printf("Connected\n");
+	}
+
+	const unsigned int BUF_LEN = 512;
+
+	char send_buf[BUF_LEN];
+	memset(send_buf, 0, BUF_LEN);
+
+	printf("\nEnter username: \n");
+	std::string userName;
+	std::getline(std::cin, userName);
+
+	userName = "@" + userName;
+
+	if (sendto(cli_socket, (char*) userName.c_str(), BUF_LEN, 0,
+		ptr->ai_addr, ptr->ai_addrlen) == SOCKET_ERROR) {
+		printf("sendto() failed %d\n", WSAGetLastError());
+		return 1;
+	}
+
+	for (;;) {
+		printf("Enter message: ");
+		std::string line;
+		std::getline(std::cin, line);
+		line += "\n";
+		char* message = (char*)line.c_str();
+
+		// send msg to server
+
+		if (sendto(cli_socket, message, BUF_LEN, 0,
+			ptr->ai_addr, ptr->ai_addrlen) == SOCKET_ERROR) {
+			printf("sendto() failed %d\n", WSAGetLastError());
+			return 1;
+		}
+
+		printf("Message sent...\n");
+
+
+	}
+
+
+	//Shutdown the socket
+
+	if (shutdown(cli_socket, SD_BOTH) == SOCKET_ERROR) {
+		printf("Shutdown failed!  %d\n", WSAGetLastError());
+		closesocket(cli_socket);
+		WSACleanup();
+		return 1;
+	}
+
+	closesocket(cli_socket);
+	freeaddrinfo(ptr);
+	WSACleanup();
+
+	return 0;
+}
---
+++ Assignment1/Server/Server.cpp
@@ -0,0 +1,132 @@
+///// UDP Server
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+#include <utility>
+#include <string>
+#include <thread>
+#include <vector>
+
+#pragma comment(lib, "Ws2_32.lib")
+int multiThreading();
+
+int main() {
+
+
+	std::thread multiThread(multiThreading);
+	multiThread.join();
+	return 0;
+}
+
+int multiThreading() {
+	//Initialize winsock
+	WSADATA wsa;
+	SYSTEMTIME stime;
+	int error;
+	error = WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	if (error != 0) {
+		printf("Failed to initialize %d\n", error);
+		return 1;
+	}
+
+	//Create a Server socket
+
+	struct addrinfo* ptr = NULL, hints;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_flags = AI_PASSIVE;
+
+	if (getaddrinfo(NULL, "8888", &hints, &ptr) != 0) {
+		printf("Getaddrinfo failed!! %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKET server_socket;
+
+	server_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if (server_socket == INVALID_SOCKET) {
+		printf("Failed creating a socket %d\n", WSAGetLastError());
+		WSACleanup();
+		return 1;
+	}
+
+	// Bind socket
+
+	if (bind(server_socket, ptr->ai_addr, (int)ptr->ai_addrlen) == SOCKET_ERROR) {
+		printf("Bind failed: %d\n", WSAGetLastError());
+		closesocket(server_socket);
+		freeaddrinfo(ptr);
+		WSACleanup();
+		return 1;
+	}
+
+	printf("Waiting for Data...\n");
+
+	// Receive msg from client
+	const unsigned int BUF_LEN = 512;
+
+	char recv_buf[BUF_LEN];
+	char ipBuff[BUF_LEN];
+
+	// Struct that will hold the IP address of the client that sent the message (we don't have accept() anymore to learn the address)
+	struct sockaddr_in fromAddr;
+	char* IPAdd;
+	int fromlen;
+	fromlen = sizeof(fromAddr);
+	/*first is IP second is username*/
+	std::vector<std::pair<std::string, std::string>> ipUsername;
+
+
+
+	for (;;) {
+
+		GetLocalTime(&stime);
+
+		memset(recv_buf, 0, BUF_LEN);
+		if (recvfrom(server_socket, recv_buf, sizeof(recv_buf), 0, (struct sockaddr*) & fromAddr, &fromlen) == SOCKET_ERROR) {
+			printf("recvfrom() failed...%d\n", WSAGetLastError());
+			return 1;
+		}
+		
+		std::string ip = inet_ntop(fromAddr.sin_family, &fromAddr, ipBuff, sizeof(ipBuff));
+
+		if (recv_buf[0] == '@') {
+			
+			printf("%s Has joined\n", recv_buf);
+			ipUsername.push_back(std::make_pair<std::string, std::string>(ip.c_str() ,recv_buf));
+
+		}
+		else {
+			int index = -1;
+
+			for (int i = 0; i < ipUsername.size(); i++) {
+				if (ipUsername[i].first == ip.c_str()) {
+					index = i;
+					break;
+				}
+			}
+
+			if (index >= 0) {
+
+				printf("(%02d:%02d) ", stime.wHour, stime.wMinute);
+				printf("%s: ", ipUsername[index].second.c_str());
+				printf("%s \n", recv_buf);
+			}
+		}
+
+		char ipbuf[INET_ADDRSTRLEN];
+		//		printf("Dest IP address: %s\n", inet_ntop(AF_INET, &fromAddr, ipbuf, sizeof(ipbuf)));
+		//		printf("Source IP address: %s\n", inet_ntop(AF_INET, &fromAddr, ipbuf, sizeof(ipbuf)));
+
+	}
+	closesocket(server_socket);
+	freeaddrinfo(ptr);
+	WSACleanup();
+	return 0;
+}
